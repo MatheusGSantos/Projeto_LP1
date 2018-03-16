42ad44f80819db0920aa24d4592bf1fe7bd09c39
MatheusGSantos <36708181+MatheusGSantos@users.noreply.github.com>
2018-03-16 20:18:34 -0300

    Mudança na função

--- Cadastro/manipulcao_arq.c
+++ Cadastro/manipulcao_arq.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 void read_data_struct(tCadastro *aCadastro, FILE *file){
-	int i = 0;
-	if( !fread(aCadastro[i],sizeof(tCadastro),1,file) )
+	if( !fread(aCadastro,sizeof(tCadastro),1,file) )
 		printf("Arquivo vazio, 0 itens importados\n");
 	else{
-		i++;
-		while(fread(aCadastro[i],sizeof(tCadastro),1,file)){
-			i++;
+		aCadastro++;
+		while(fread(aCadastro,sizeof(tCadastro),1,file)){
+			aCadastro++;;
 		}
 	}	
 }
