4d48f96929d9e086242e2a3dc2cf0ee0fcc4c7fc
jack <36930468+jackineo@users.noreply.github.com>
2018-03-16 21:21:53 -0300

    Add files via upload

---
+++ struct.c
@@ -0,0 +1,21 @@
+
+typedef struct Cadastro
+{
+  int ID;
+  char rua[50];
+  int numero;
+  char bairro[30];
+  char CEP[11];
+  char cidade[20];
+  double valor;
+  char disp[10];
+  char tipo[12];
+  double area;
+  double areaC;
+  int quartos;
+  int pavimentos;
+  double aluguel;
+  char posicao[50];
+  int garagem;
+  
+}tCadastro;
