114ca5a932eaa2fe9ad3c9ad6a69f67f08938152
MatheusGSantos <36708181+MatheusGSantos@users.noreply.github.com>
2018-03-30 19:29:34 -0300

    Add files via upload

--- Cadastro/struct.c
+++ Cadastro/struct.c
@@ -1,5 +1,58 @@
+typedef struct
+{
+  int N_pav;
+  int N_quartos;
+  char posicao [16];
+  double area_terreno;
+  double area_construida;
+}tCasa;
 
-typedef struct Cadastro
+
+typedef struct
+{
+  double area;
+  int N_quartos;
+  char posicao[16];
+  int N_andar;
+  double valor_cond;
+  int vagas_garag;
+}tApartamento;
+
+
+typedef struct
+{
+  double area;
+}tTerreno;
+
+
+typedef struct
+{
+  double area;
+  double condominio;
+  char arCond[4];
+  char int_tv[4];
+  char lavanderia[4];
+  char limpeza[4];
+  char atend24[4];
+}tFlat;
+
+
+typedef struct
+{
+  double area;
+  double condominio;
+  char arCond[4];
+  char int_tv[4];
+  char lavanderia[4];
+  char limpeza[4];
+  char atend24[4];
+  char pscina[4];
+  char sauna[4];
+  char ginastica[4];
+}tStudio;
+
+
+typedef struct
 {
   int ID;
   char rua[50];
@@ -10,12 +63,9 @@ typedef struct Cadastro
   double valor;
   char disp[10];
   char tipo[12];
-  double area;
-  double areaC;
-  int quartos;
-  int pavimentos;
-  double aluguel;
-  char posicao[50];
-  int garagem;
-  
+  tCasa casa;
+  tApartamento apartamento;
+  tTerreno terreno;
+  tFlat flat;
+  tStudio studio;
 }tCadastro;
