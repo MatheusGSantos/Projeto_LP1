1bc578dcc1c7c0bdab09f02287bb1f83a5def3db
MatheusGSantos <36708181+MatheusGSantos@users.noreply.github.com>
2018-04-01 17:11:40 -0300

    Update Cadastro.c

--- Cadastro/Cadastro.c
+++ Cadastro/Cadastro.c
@@ -74,6 +74,23 @@ typedef struct
   tStudio studio;
 }tCadastro;
 
+void read_data_struct(tCadastro *aCadastro, FILE *file){
+	if( !fread(aCadastro,sizeof(tCadastro),1,file) )
+		printf("Arquivo vazio, 0 itens importados\n");
+	else{
+		aCadastro++;
+		while(fread(aCadastro,sizeof(tCadastro),1,file)){
+			aCadastro++;
+		}
+	}	
+}
+
+void save_data_struct(tCadastro *aCadastro, FILE *file){
+	while( fwrite(aCadastro,1,sizeof(tCadastro),file) ){
+		aCadastro++;
+	}
+}
+
 int change_index(tCadastro *array){
   int counter = 0;
   while(1){
