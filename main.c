fbf164fe0fb778c43db1ac7c8148edabde1a963b
Matheus Gama dos Santos <36708181+MatheusGSantos@users.noreply.github.com>
2018-04-02 20:27:08 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
+#include <Windows.h>
 #include <Cadastro.h>
 #include <Consulta.h>
 
@@ -25,31 +26,24 @@ int main()
     if(op == 1){
         system("cls");
         printf("\t\t\t\tCadastrar\n\n");
-        while(1){
+        Cadastro:
             //rodar função de cadastro
             printf("Deseja cadastrar outro?  1-Sim   2-Nao\nR: ");
             scanf("%d", &op);
 
             if(op == 1){
-               continue;
+               goto Cadastro;
             }else{
-                printf("\n1-Fechar  2-Menu Principal\nR: ");
-                scanf("%d", &op);
-                if(op == 1)
-                    break;
-                else
-                    goto Menu;
+                goto Menu;
             }
-        }
     }else if(op == 2){
         system("cls");
 
         char imov;
 
-        while(1){
+        Consulta:
             system("cls");
-            printf("\t\t\t\tConsultar\n\nQual tipo de imovel deseja consultar?  R: ");
-            scanf("%s", &imov);
+            printf("\n\nR: ");
 
             //Function Consultation
 
@@ -57,17 +51,15 @@ int main()
             scanf("%d", &op);
 
             if(op == 1){
-                continue;
+                goto Consulta;
             }else{
                 goto Menu;
             }
-         }
-
     }else{
         
         fclose(file);
         printf("Fechando programa...\n\n");
-        delay(3000);
+        Sleep(3000);
         return 0;
     }
     return 0;
