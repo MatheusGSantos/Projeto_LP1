a287f1d2dd06f797da2c11520005ef6ce1f21496
Matheus_Gama <mths.gm@gmail.com>
2018-05-20 13:01:59 -0300

    .

--- C++/files/casa.cpp
+++
@@ -1,9 +0,0 @@
-#include "Casa.h"
-#include <iostream>
-
-Casa::Casa(int numPavim, int numQuartos, double areaTerreno, double areaConstruida):Imovel(){
-	this->numPavim = numPavim;
-	this->numQuartos = numQuartos;
-	this->areaTerreno = areaTerreno;
-	this->areaConstruida = areaConstruida;
-}
